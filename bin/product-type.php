d95d540b5b4fee229cdf9235f5801b6b7c52acb2
openthc <code@openthc.com>
2019-09-20 19:29:21 +0000

    canonical product types

---
+++ bin/product-type.php
@@ -0,0 +1,147 @@
+#!/usr/bin/php
+<?php
+/**
+ * Product Type Toolkit
+ */
+
+if (empty($argv[1])) {
+	echo "Say one of 'csv', 'tsv', 'json', or 'sql'\n";
+	exit(1);
+}
+
+$d = __DIR__;
+$d = dirname($d);
+define('APP_ROOT', $d);
+
+$src_path = $d . '/etc/product-type';
+$src_list = glob("$src_path/*.yaml");
+$src_data = [];
+foreach ($src_list as $src_file) {
+	// $yaml = file_get_contents($src_file);
+	$src_data[] = yaml_parse_file($src_file, 0);
+}
+
+switch ($argv[1]) {
+case 'csv':
+case 'tsv':
+
+	$sep = ',';
+	if ('tsv' == $argv[1]) {
+		$sep = "\t";
+	}
+
+	$fh = fopen('php://output', 'a');
+
+	foreach ($src_data as $out_data) {
+
+		$rec = array(
+			$out_data['id'],
+			$out_data['name'],
+			$out_data['stub'],
+			$out_data['sort'],
+			$out_data['biotrack']['path'],
+			$out_data['leafdata']['path'],
+			$out_data['metrc']['path'],
+		);
+
+		fputcsv($fh, $rec, $sep);
+	}
+
+	fclose($fh);
+
+	exit(0);
+
+	break;
+
+case 'json':
+
+	echo json_encode($src_data, JSON_PRETTY_PRINT);
+
+	exit(0);
+
+	break;
+
+case 'sql':
+
+	foreach ($src_data as $out_data) {
+
+		$out_data['meta'] = json_encode([
+			'biotrack' => $out_data['biotrack'],
+			'leafdata' => $out_data['leafdata'],
+			'metrc' => $out_data['metrc'],
+		]);
+
+		echo "INSERT INTO product_type (id, name, stub, sort, meta) VALUES (";
+		echo "'{$out_data['id']}',";
+		echo "'{$out_data['name']}',";
+		echo "'{$out_data['stub']}',";
+		echo "'{$out_data['sort']}',";
+		echo "'{$out_data['meta']}'";
+		echo ");\n";
+
+	}
+
+	// $dbc = new PDO(sprintf('sqlite:%s/product-type.sql', $d));
+	// $dbc->query('CREATE TABLE product_type (id, code, name, mode, unit)');
+	//
+	// $ins = $dbc->prepare('INSERT INTO product_type VALUES (?, ?, ?, ?)');
+	//
+	// 	$ins->execute(array(
+	// 		$out_data['id'],
+	// 		$out_data['name'],
+	// 		$out_data['mode'],
+	// 		$out_data['unit'],
+	// 	));
+	//
+	// }
+
+	break;
+
+case 'yaml':
+
+	$d = date(DateTime::RFC3339);
+
+	foreach ($src_data as $out_data) {
+
+		$out_file = sprintf('%s/etc/product-type/%s.yaml', APP_ROOT, $out_data['id']);
+
+		echo "Ulid: {$out_data['id']}\n";
+		if (empty($out_data['stub'])) {
+			$out_data['stub'] = _text_stub($out_data['name']);
+		}
+
+		// $out_data = [];
+		// $out_data['id'] = $ulid;
+		// $out_data['name'] = $pt_data['name']; unset($pt_data['name']);
+		// $out_data['mode'] = $pt_data['mode']; unset($pt_data['mode']);
+		// $out_data['sort'] = $pt_data['sort']; unset($pt_data['sort']);
+		// $out_data['stub'] = null;
+		// $out_data['biotrack'] = []; unset($pt_data['biotrack']);
+		// $out_data['biotrack']['path'] = $pt_data['biotrack_code']; unset($pt_data['biotrack_code']);
+		// $out_data['biotrack']['name'] = null;
+		// $out_data['leafdata'] = []; unset($pt_data['leafdata']);
+		// $out_data['leafdata']['path'] = $pt_data['leafdata_code']; unset($pt_data['leafdata_code']);
+		// $out_data['leafdata']['name'] = null;
+		// $out_data['metrc'] = []; unset($pt_data['metrc']);
+		// $out_data['metrc']['path'] = $pt_data['metrc_code']; unset($pt_data['metrc_code']);
+		// $out_data['metrc']['name'] = null;
+
+
+		$out_data = yaml_emit($out_data, YAML_UTF8_ENCODING, YAML_LN_BREAK);
+		$out_data = "#\n# Generated File $d\n#\n\n$out_data\n";
+		echo "Output To: $out_file\n";
+
+		// file_put_contents($out_file, $out_data);
+
+	}
+
+}
+
+function _text_stub($x)
+{
+	$x = strtolower($x);
+	$x = preg_replace('/[^\w\-]+/', '-', $x);
+	$x = preg_replace('/\-+/', '-', $x);
+	$x = trim($x, '-');
+	return $x;
+}
