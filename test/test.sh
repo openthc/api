76cf1a24a46c1262257e402d0153d7f900028a8f
openthc <code@openthc.com>
2020-03-19 18:47:13 +0000

    update test config

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,41 @@
-#!/bin/bash
+#!/bin/bash -x
 #
-# Run the Test Cases
+# OpenTHC Test Runner
 #
+
+set -o errexit
+set -o nounset
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
+dt=$(date)
+dts=$(date +%Y%m%d-%H%M%S)
 
 cd "$d"
 
+out_path="../webroot/test-output"
+if [ ! -d "$out_path" ]
+then
+	mkdir "$out_path"
+fi
 
-../vendor/bin/phpunit
 
-xsltproc ./style.xsl ../webroot/reports/phpunit.xml > "../webroot/reports/phpunit.html"
+#
+#
+../vendor/bin/phpunit "$@" 2>&1 | tee "$out_path/output.txt"
+note=$(tail -n1 "$out_path/output.txt")
+
+#
+# Get Transform
+if [ ! -f "phpunit-report.xsl" ]
+then
+	wget https://openthc.com/css/phpunit-report.xsl
+fi
+
+xsltproc \
+	--nomkdir \
+	--output "$out_path/output.html" \
+	phpunit-report.xsl \
+	"$out_path/output.xml"
+
+sed "s/%TEST_DATE%/$dt/g; s/%TEST_NOTE%/$note/g;" ./report.html > ../webroot/test-output/index.html
