5c74e53abfad886cc28ae3447cefbc39c21f3581
openthc <code@openthc.com>
2021-01-09 23:05:39 +0000

    patchup test

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,7 @@
 
 set -o errexit
 set -o nounset
+# set -o pipefail
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
@@ -12,29 +13,72 @@ dt=$(date)
 
 cd "$d"
 
-out_path="../webroot/test-output"
-if [ ! -d "$out_path" ]
+report_path="../webroot/test-output"
+if [ ! -d "$report_path" ]
 then
-	mkdir "$out_path"
+	mkdir "$report_path"
 fi
 
+report_home="$report_path/index.html"
+
+echo '<h1>Tests Started</h1>' > "$report_home"
+
 
 #
 #
-../vendor/bin/phpunit "$@" 2>&1 | tee "$out_path/output.txt"
-note=$(tail -n1 "$out_path/output.txt")
+../vendor/bin/phpunit \
+	--log-junit "$report_path/output.xml" \
+	--testdox-html "$report_path/testdox.html" \
+	--testdox-text "$report_path/testdox.txt" \
+	--testdox-xml "$report_path/testdox.xml" \
+	--verbose \
+	"$@" 2>&1 | tee "$report_path/output.txt"
+
+# if [[ $ret != 0 ]]
+# then
+# 	echo "PHPUnit Failed"
+# 	exit 1;
+# fi
+note=$(tail -n1 "$report_path/output.txt")
+
+echo '<h1>Tests Completed</h1>' > "$report_home"
 
 #
 # Get Transform
-if [ ! -f "report.xsl" ]
-then
-	wget -q https://openthc.com/pub/phpunit/report.xsl
-fi
-
+echo '<h1>Transforming...</h1>' > "$report_home"
+curl -qs https://openthc.com/pub/phpunit/report.xsl > report.xsl
 xsltproc \
 	--nomkdir \
-	--output "$out_path/output.html" \
+	--output "$report_path/output.html" \
 	report.xsl \
-	"$out_path/output.xml"
+	"$report_path/output.xml"
 
-sed "s/%TEST_DATE%/$dt/g; s/%TEST_NOTE%/$note/g;" ./report.html > ../webroot/test-output/index.html
+#
+# Final Ouptut
+cat <<HTML > "$report_home"
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1, user-scalable=yes">
+<meta name="theme-color" content="#247420">
+<link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
+<title>Test Result $dt</title>
+</head>
+<body>
+<div class="container mt-4">
+<div class="jumbotron">
+
+<h1>Test Result $dt</h1>
+<h2>$note</h2>
+
+<p>You can view the <a href="output.txt">raw script output</a>,
+or the <a href="output.xml">Unit Test XML</a>
+which we've processed <small>(via XSL)</small> to <a href="output.html">a pretty report</a>
+which is also in <a href="testdox.html">testdox format</a>.
+</p>
+
+</div>
+</div>
+</body>
+</html>
+HTML
