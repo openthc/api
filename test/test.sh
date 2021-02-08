914d3a05dba12b01d53c98a2d7b2cbc5c0fd1774
openthc <code@openthc.com>
2021-02-08 20:53:46 +0000

    test updates

--- test/test.sh
+++ test/test.sh
@@ -9,7 +9,6 @@ set -o nounset
 
 f=$(readlink -f "$0")
 d=$(dirname "$f")
-dt=$(date)
 
 cd "$d"
 
@@ -20,43 +19,40 @@ mkdir -p "$output_base"
 
 #
 # Lint
-echo '<h1>Linting</h1>' > "$output_main"
+echo '<h1>Linting...</h1>' > "$output_main"
 find ../api/ ../bin/ ../lib/ ../sbin/ ../view/ -type f -name '*.php' -exec php -l {} \; \
 	| grep -v 'No syntax' || true \
 	2>&1 >"$output_base/phplint.txt"
 
 
 #
-#
+# PHPUnit
+echo '<h1>Running Tests...</h1>' > "$output_main"
 ../vendor/bin/phpunit \
+	--verbose \
 	--log-junit "$output_base/output.xml" \
 	--testdox-html "$output_base/testdox.html" \
 	--testdox-text "$output_base/testdox.txt" \
 	--testdox-xml "$output_base/testdox.xml" \
-	--verbose \
 	"$@" 2>&1 | tee "$output_base/output.txt"
 
-# if [[ $ret != 0 ]]
-# then
-# 	echo "PHPUnit Failed"
-# 	exit 1;
-# fi
-note=$(tail -n1 "$output_base/output.txt")
-
-echo '<h1>Tests Completed</h1>' > "$output_main"
 
 #
-# Get Transform
+# Transform
 echo '<h1>Transforming...</h1>' > "$output_main"
-curl -qs https://openthc.com/pub/phpunit/report.xsl > report.xsl
+[ -f "report.xsl" ] || wget -q 'https://openthc.com/pub/phpunit/report.xsl'
 xsltproc \
 	--nomkdir \
 	--output "$output_base/output.html" \
 	report.xsl \
 	"$output_base/output.xml"
 
+
 #
-# Final Ouptut
+# Final Output
+dt=$(date)
+note=$(tail -n1 "$output_base/output.txt")
+
 cat <<HTML > "$output_main"
 <html>
 <head>
@@ -64,14 +60,14 @@ cat <<HTML > "$output_main"
 <meta name="viewport" content="initial-scale=1, user-scalable=yes">
 <meta name="theme-color" content="#247420">
 <link rel="stylesheet" href="https://cdn.openthc.com/bootstrap/4.4.1/bootstrap.css" integrity="sha256-L/W5Wfqfa0sdBNIKN9cG6QA5F2qx4qICmU2VgLruv9Y=" crossorigin="anonymous">
-<title>Test Result $dt</title>
+<title>Test Result ${dt}</title>
 </head>
 <body>
 <div class="container mt-4">
 <div class="jumbotron">
 
-<h1>Test Result $dt</h1>
-<h2>$note</h2>
+<h1>Test Result ${dt}</h1>
+<h2>${note}</h2>
 
 <p>You can view the <a href="output.txt">raw script output</a>,
 or the <a href="output.xml">Unit Test XML</a>
